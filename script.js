ec62de814d01e1a86de27cff6f24366bc66569c4
boy1434@naver.com <boy1434@naver.com>
2024-02-14 01:02:36 +0900

    할일 앱 -3 체크 버튼,리턴버튼

--- script.js
+++ script.js
@@ -2,35 +2,85 @@
 // + 버튼을 클릭하면, 할일이 추가 된다
 // delete 버튼을 누르면 할일이 삭제된다
 // check 버튼을 누르면 할일이 끝나면서 밑줄이 가게된다
+// 1. check 버튼을 클릭하는 순간 true <= false
 // 진행중 끝남 탭을 누르면 언더바가 이동한다
 // 끝난탭은 끝난 아이템만, 진행중ㄷ 탭은 진행중 아이템만
 // 전체ㅐㅂ을 누르면 다시 전체아이템으로 돌아옴
 
 let taskInput = document.getElementById("task-input");
-let plusBtn = document.getElementById("add-btn");
+let addBtn = document.getElementById("add-btn");
 let taskList = [];
 
-plusBtn.addEventListener("click", addTask);
+addBtn.addEventListener("click", addTask);
 
+// + 버튼을 눌렀을때 값이 저장??? 되는 곳?!
 function addTask() {
-    let taskContent = taskInput.value;
-    taskList.push(taskContent);
+    let task = {
+        id:randomIDGenerate(),
+        taskContent: taskInput.value,
+        isComplete:false,
+    }
+    taskList.push(task);
     console.log(taskList);
     render();
 }
 
+// 플러스 버튼을 눌렀을때 보여지는 곳
 function render(){
     let resultHTML = '';
     for(let i=0; i<taskList.length; i++){
-        resultHTML += `<div class="task">
-        <div>${taskList[i]}</div>
+
+        if(taskList[i].isComplete == true){
+            resultHTML += `<div class="task">
+            <div class="task-done">${taskList[i].taskContent}</div>
+            <div>
+                <button class="returnBtn" onclick="returnBtn('${taskList[i].id}')"><i class="fa-solid fa-arrow-rotate-left"></i></button>
+                <button >delete</button>
+            </div>
+        </div>`
+        } else if(taskList[i].isComplete == false){ 
+            resultHTML += `<div class="task">
+        <div>${taskList[i].taskContent}</div>
         <div>
-            <button>Check</button>
-            <button>delete</button>
+            <button class="checkBtn"onclick="checkBtn('${taskList[i].id}')"><i class="fa-solid fa-check"></i></button>
+            <button >delete</button>
         </div>
     </div>`;
+        }
+
+        
     }
     
     document.getElementById("task-board").innerHTML = resultHTML;
     
+}
+
+function returnBtn(id){
+    console.log('id:,',id)
+    for(let i=0; i<taskList.length; i++){
+        if(taskList[i].id == id){
+            taskList[i].isComplete = false;
+            break;
+        }
+    }
+    
+    render();
+    console.log(taskList);
+}
+
+function checkBtn(id){
+    console.log('id:,',id)
+    for(let i=0; i<taskList.length; i++){
+        if(taskList[i].id == id){
+            taskList[i].isComplete = true;
+            break;
+        }
+    }
+    
+    render();
+    console.log(taskList);
+}
+
+function randomIDGenerate(){
+    return '_' + Math.random().toString(36).substring(2, 9);
 }
